82e768212714afd9b40c53c894de824ae7124a5f
JyothiDevarapalli <125997684+JyothiDevarapalli@users.noreply.github.com>
2023-12-04 19:25:42 +0100

    Speech Emotion Recognition.py

--- Speech Emotion Recognition.py
+++ Speech Emotion Recognition.py
@@ -1,27 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 
 import pandas as pd
 import numpy as np
 import os
 import sys
-
-# librosa is a Python library for analyzing audio and music. It can be used to extract the data from the audio files we will see it later.
 import librosa
 import librosa.display
 import seaborn as sns
 import matplotlib.pyplot as plt
-
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
-
-# to play the audio files
 from IPython.display import Audio
-
 import keras
 from keras.callbacks import ReduceLROnPlateau
 from keras.models import Sequential
@@ -34,19 +23,11 @@ if not sys.warnoptions:
     warnings.simplefilter("ignore")
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
-
-# In[ ]:
-
-
 # Paths for data.
 Ravdess = "File path of Ravdess emotional speech audio "
 Crema = "File path of AudioWAV"
 Tess = "File path of TESS Toronto emotional speech set data"
 Savee = "File path of Surrey Audio Visual Expressed Emotion"
-
-
-# In[ ]:
-
 
 #RAVDESS Dataset
 ravdess_directory_list = os.listdir(Ravdess)
@@ -54,19 +35,15 @@ ravdess_directory_list = os.listdir(Ravdess)
 file_emotion = []
 file_path = []
 for dir in ravdess_directory_list:
-    # as their are 20 different actors in our previous directory we need to extract files for each actor.
     actor = os.listdir(Ravdess + dir)
     for file in actor:
         part = file.split('.')[0]
         part = part.split('-')
-        # third part in each file represents the emotion associated to that file.
         file_emotion.append(int(part[2]))
         file_path.append(Ravdess + dir + '/' + file)
         
-# dataframe for emotion of files
 emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
 
-# dataframe for path of files.
 path_df = pd.DataFrame(file_path, columns=['Path'])
 Ravdess_df = pd.concat([emotion_df, path_df], axis=1)
 
@@ -76,14 +53,10 @@ Ravdess_df.head()
 
 #CREMA dataset
 crema_directory_list = os.listdir(Crema)
-
 file_emotion = []
 file_path = []
-
 for file in crema_directory_list:
-    # storing file paths
     file_path.append(Crema + file)
-    # storing file emotions
     part=file.split('_')
     if part[2] == 'SAD':
         file_emotion.append('sad')
@@ -120,10 +93,7 @@ for dir in tess_directory_list:
             file_emotion.append(part)
         file_path.append(Tess + dir + '/' + file)
         
-# dataframe for emotion of files
 emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
-
-# dataframe for path of files.
 path_df = pd.DataFrame(file_path, columns=['Path'])
 Tess_df = pd.concat([emotion_df, path_df], axis=1)
 Tess_df.head()
@@ -133,7 +103,6 @@ savee_directory_list = os.listdir(Savee)
 
 file_emotion = []
 file_path = []
-
 for file in savee_directory_list:
     file_path.append(Savee + file)
     part = file.split('_')[1]
@@ -160,19 +129,9 @@ emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
 path_df = pd.DataFrame(file_path, columns=['Path'])
 Savee_df = pd.concat([emotion_df, path_df], axis=1)
 Savee_df.head()
-
-
-# In[ ]:
-
-
-# creating Dataframe using all the 4 dataframes we created so far.
 data_path = pd.concat([Ravdess_df, Crema_df, Tess_df, Savee_df], axis = 0)
 data_path.to_csv("data_path.csv",index=False)
 data_path.head()
-
-
-# In[ ]:
-
 
 #Data Exploraion and Visualization
 plt.title('Count of Emotions', size=16)
@@ -195,12 +154,7 @@ def create_spectrogram(data, sr, e):
     plt.figure(figsize=(12, 3))
     plt.title('Spectrogram for audio with {} emotion'.format(e), size=15)
     librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')   
-    #librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
     plt.colorbar()
-
-
-# In[ ]:
-
 
 emotion='fear'
 path = np.array(data_path.Path[data_path.Emotions==emotion])[1]
@@ -230,12 +184,7 @@ create_waveplot(data, sampling_rate, emotion)
 create_spectrogram(data, sampling_rate, emotion)
 Audio(path)
 
-
-# In[ ]:
-
-
 # Data Augumentation
-
 def noise(data):
     noise_amp = 0.035*np.random.uniform()*np.amax(data)
     data = data + noise_amp*np.random.normal(size=data.shape[0])
@@ -284,41 +233,33 @@ plt.figure(figsize=(14,4))
 librosa.display.waveplot(y=x, sr=sample_rate)
 Audio(x, rate=sample_rate)
 
-
-# In[ ]:
-
-
 #Feature Extraction
 def extract_features(data):
     # ZCR
     result = np.array([])
     zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
-    result=np.hstack((result, zcr)) # stacking horizontally
+    result=np.hstack((result, zcr)) 
 
     # Chroma_stft
     stft = np.abs(librosa.stft(data))
     chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
-    result = np.hstack((result, chroma_stft)) # stacking horizontally
+    result = np.hstack((result, chroma_stft)) 
 
     # MFCC
     mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
-    result = np.hstack((result, mfcc)) # stacking horizontally
+    result = np.hstack((result, mfcc)) 
 
     # Root Mean Square Value
     rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
-    result = np.hstack((result, rms)) # stacking horizontally
+    result = np.hstack((result, rms)) 
 
     # MelSpectogram
     mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
-    result = np.hstack((result, mel)) # stacking horizontally
-    
+    result = np.hstack((result, mel)) 
     return result
 
 def get_features(path):
-    # duration and offset are used to take care of the no audio in start and the ending of each audio files as seen above.
     data, sample_rate = librosa.load(path, duration=2.5, offset=0.6)
-    
-    # without augmentation
     res1 = extract_features(data)
     result = np.array(res1)
     
@@ -332,20 +273,13 @@ def get_features(path):
     data_stretch_pitch = pitch(new_data, sample_rate)
     res3 = extract_features(data_stretch_pitch)
     result = np.vstack((result, res3)) # stacking vertically
-    
     return result
-
-
-
-# In[ ]:
-
 
 X, Y = [], []
 for path, emotion in zip(data_path.Path, data_path.Emotions):
     feature = get_features(path)
     for ele in feature:
         X.append(ele)
-        # appending emotion 3 times as we have made 3 augmentation techniques on each audio file.
         Y.append(emotion)
         
 len(X), len(Y), data_path.Path.shape
@@ -354,15 +288,9 @@ Features['labels'] = Y
 Features.to_csv('features.csv', index=False)
 Features.head()
 
-
-# In[ ]:
-
-
 # Data Preperation
 X = Features.iloc[: ,:-1].values
 Y = Features['labels'].values
-
-# As this is a multiclass classification problem onehotencoding our Y.
 encoder = OneHotEncoder()
 Y = encoder.fit_transform(np.array(Y).reshape(-1,1)).toarray()
 
@@ -381,12 +309,7 @@ x_train = np.expand_dims(x_train, axis=2)
 x_test = np.expand_dims(x_test, axis=2)
 x_train.shape, y_train.shape, x_test.shape, y_test.shape
 
-
-# In[ ]:
-
-
 #Modelling
-
 model=Sequential()
 model.add(Conv1D(256, kernel_size=5, strides=1, padding='same', activation='relu', input_shape=(x_train.shape[1], 1)))
 model.add(MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
@@ -437,9 +360,6 @@ ax[1].set_xlabel("Epochs")
 plt.show()
 
 
-# In[ ]:
-
-
 # predicting on test data.
 pred_test = model.predict(x_test)
 y_pred = encoder.inverse_transform(pred_test)
@@ -451,9 +371,6 @@ df['Predicted Labels'] = y_pred.flatten()
 df['Actual Labels'] = y_test.flatten()
 
 df.head(10)
-
-
-# In[ ]:
 
 
 cm = confusion_matrix(y_test, y_pred)
